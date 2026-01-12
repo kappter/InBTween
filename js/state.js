c776da554e8da60d056503ba34fa3995dcbebf49
Ken Kapptie <kkapptie@graniteschools.org>
2026-01-12 12:21:05 -0700

    Add sessionState object to manage conversation state

---
+++ js/state.js
@@ -0,0 +1,11 @@
+export const sessionState = {
+  topic: "",
+  phase: "intro", // intro | persona | clarification | direct
+  participants: [],
+  personas: [],
+  dialogueLog: [],
+  acknowledgements: {
+    participantA: false,
+    participantB: false
+  }
+};
